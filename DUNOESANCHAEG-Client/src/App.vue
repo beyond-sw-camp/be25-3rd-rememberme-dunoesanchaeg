0b0f855d27a3479ba6c782b8821b8711aa9dad26
JS-Yang <junsuk530@gmail.com>
2026-03-31 12:27:28 +0900

    feat:접근성 설정 전역 상태관리 추가

--- DUNOESANCHAEG-Client/src/App.vue
+++ DUNOESANCHAEG-Client/src/App.vue
@@ -6,10 +6,3 @@
 import MainLayout from './layouts/MainLayout.vue';
 </script>
 
-<style>
-body {
-  margin: 0;
-  padding: 0;
-  font-family: 'Pretendard', -apple-system, BlinkMacSystemFont, system-ui, Roboto, sans-serif;
-}
-</style>
