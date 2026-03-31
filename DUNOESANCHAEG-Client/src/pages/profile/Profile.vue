7d75ae9272eb35b130855ae5ce5d228e0a9c25f5
horolo1234 <qkrwogk2233@gmail.com>
2026-03-31 15:49:44 +0900

    refactor: 이미지 수정 및 크기 수정

--- DUNOESANCHAEG-Client/src/pages/profile/Profile.vue
+++ DUNOESANCHAEG-Client/src/pages/profile/Profile.vue
@@ -2,7 +2,7 @@
   <div class="min-h-screen bg-brand-bg p-6 pb-28">
 
     <header class="flex items-center gap-3 mb-10">
-      <van-image :src="logoGreen" alt="두뇌산책 로고" class="w-20 h-auto" />
+      <van-image :src="logoGreen" alt="두뇌산책 로고" class="w-10 h-auto" />
       <h1 class="text-3xl font-black text-brand-green tracking-tight">두뇌산책</h1>
     </header>
 
