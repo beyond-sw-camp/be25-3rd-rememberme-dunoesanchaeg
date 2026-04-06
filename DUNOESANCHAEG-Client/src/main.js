b8e6defa45d8926c44aa6b687b6d7376bf1d8da2
JS-Yang <junsuk530@gmail.com>
2026-04-06 17:40:05 +0900

    chore: fonts 적용되게 수정

--- DUNOESANCHAEG-Client/src/main.js
+++ DUNOESANCHAEG-Client/src/main.js
@@ -2,12 +2,14 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import './assets/main.css';
 
 import { createPinia } from 'pinia';
 
-// 1. Vant UI 스타일 전체 불러오기
+// 1. Vant UI 스타일 전체 불러오기 (우선 적용)
 import 'vant/lib/index.css';
+
+// 2. 커스텀 CSS (Vant 스타일을 덮어쓰기 위해 Vant 아래에 선언)
+import './assets/main.css';
 
 // 2. 사용할 컴포넌트들 불러오기
 import { NavBar, Icon, Tabbar, TabbarItem, Progress, Tag } from 'vant';
