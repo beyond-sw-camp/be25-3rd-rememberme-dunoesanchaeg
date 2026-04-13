37da84bd9eaf479eaa3ce665dece9aed61d09083
0lthree <oithreed@gmail.com>
2026-04-13 16:59:35 +0900

    refactor: 개방형질문 url 수정

--- DUNOESANCHAEG-Client/src/api/openQuestion.js
+++ DUNOESANCHAEG-Client/src/api/openQuestion.js
@@ -2,14 +2,14 @@ import instance from '@/api/instance';
 
 export const startOpenQuestion = async () => {
     const res = await instance.post(
-        '/api/v1/open-questions/start'
+        '/open-questions/start'
     );
     return res.data.data;
 };
 
 export const completeOpenQuestion = async (dailyQuestionLogId, responseSecond) => {
     const res = await instance.patch(
-        `/api/v1/open-questions/${dailyQuestionLogId}`,
+        `/open-questions/${dailyQuestionLogId}`,
         { responseSecond }
     );
     return res.data.data;
@@ -17,7 +17,7 @@ export const completeOpenQuestion = async (dailyQuestionLogId, responseSecond) =
 
 export const exitOpenQuestion = async (dailyQuestionLogId) => {
     const res = await instance.delete(
-        `/api/v1/open-questions/${dailyQuestionLogId}`
+        `/open-questions/${dailyQuestionLogId}`
     );
     return res.data.data;
 };
