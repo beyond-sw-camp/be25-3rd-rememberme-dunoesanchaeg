77ac79472d44cd3d72907823067761ab3db23f98
heejudy <moheeju1223@gmail.com>
2026-04-14 12:31:22 +0900

    하은님 api 주소 변경(/api/v1 삭제)

--- DUNOESANCHAEG-Client/src/api/dailyRecord.js
+++ DUNOESANCHAEG-Client/src/api/dailyRecord.js
@@ -1,6 +1,6 @@
 import instance from '@/api/instance.js';
 
 export const saveDailyRecord = async (payload) => { 
-    const {data} = await instance.put('/api/v1/daily-record', payload);
+    const {data} = await instance.put('/daily-record', payload);
     return data;
 };
