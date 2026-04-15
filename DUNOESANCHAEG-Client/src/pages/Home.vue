fbe954fac0cce0c1ba2432e7d65ab921cb301535
heejudy <moheeju1223@gmail.com>
2026-04-15 10:44:04 +0900

    refactor: 컨페티 요소 추가

--- DUNOESANCHAEG-Client/src/pages/Home.vue
+++ DUNOESANCHAEG-Client/src/pages/Home.vue
@@ -5,6 +5,8 @@
       <span class="text-3xl font-extrabold text-brand-green">두뇌산책</span>
     </header>
 
+    <h1 @click="showConfetti"></h1>
+
     <!-- // 사용자 인사, 날짜 컴포넌트 -->
     <HomeHeader :username="username" 
                 :formattedDate="formattedDate" />
@@ -31,8 +33,11 @@ import HomeHeader from '@/pages/mainpage/HomeHeader.vue';
 import ProgressRate from './mainpage/ProgressRate.vue';
 import TodayRoutines from './mainpage/TodayRoutines.vue';
 
-import { onMounted, watch } from 'vue';
+import { onBeforeUnmount, onMounted, watch } from 'vue';
 import { useRoute } from 'vue-router';
+
+import JSConfetti from 'js-confetti';
+
 const route = useRoute();
 
 
@@ -47,6 +52,26 @@ const {
   initializeHome
 } = useHome();
 
+// 1. 인스턴스를 하나만 생성 (메모리 효율)
+const confetti = new JSConfetti();
+
+function showConfetti() {
+  confetti.addConfetti({
+    // confettiColors: ['#ff0000', '#00ff00', '#0000ff'], // 브랜드 컬러에 맞게 조정 가능
+    // confettiColors: ['#27D16A', '#DAEDFF'], // 초록, 배경색
+    confettiColors: ['#2F7431', '#8ED35D', '#FFD700'], // 초록, 연두, 금색
+    confettiNumber: 250, // 너무 많으면 렉 걸리니 적절히 조절 (기본값 150)
+    confettiRadius: 6,
+  });
+}
+
+// 2. 진행률이 100이 되었을 때만 감지해서 실행
+watch(progress, (newProgress) => {
+  if (newProgress === 100) {
+    showConfetti();
+  }
+});
+
 onMounted(() => {
   initializeHome();
 })
@@ -57,6 +82,12 @@ watch(
     initializeHome();
   }
 );
+
+onBeforeUnmount(() => {
+  const canvas = document.getElementById('confetti-canvas');
+  if (canvas) canvas.remove();
+});
+
 </script>
 
 <style scoped>
--- DUNOESANCHAEG-Client/src/pages/mainpage/HomeHeader.vue
+++ DUNOESANCHAEG-Client/src/pages/mainpage/HomeHeader.vue
@@ -1,14 +1,13 @@
 <template>
-    <section class="flex justify-between items-center">
-        <div class="py-2">
+    <section>
+        <div class="text-right text-xl font-semibold text-zinc-400 mt-2">
+                {{ formattedDate }}
+        </div>
+        <div class="py-1">
             <h2 class="text-2xl font-bold text-zinc-800">
                 반가워요, <span class="text-brand-green">{{ username }}</span> 님!
             </h2>
             <p class="text-zinc-500 font-medium">오늘도 건강하게 산책해볼까요? ☀️</p>
-        </div>
-
-        <div class="text-xl font-semibold text-zinc-400 mt-1">
-            {{ formattedDate }}
         </div>
     </section>
 </template>
