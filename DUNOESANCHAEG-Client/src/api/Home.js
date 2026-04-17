3fbaf79fddb0b521f9a05a448775e11f19551f68
heejudy <moheeju1223@gmail.com>
2026-04-17 09:40:23 +0900

    fix: api 주소 수정

--- DUNOESANCHAEG-Client/src/api/Home.js
+++ DUNOESANCHAEG-Client/src/api/Home.js
@@ -2,5 +2,5 @@ import instance from './instance';
 
 export const routineApi = {
     // 오늘 루틴 가져오기
-    getTodayRoutine: () => instance.get('/api/v1/routines/today')
+    getTodayRoutine: () => instance.get('/routines/today')
 };
